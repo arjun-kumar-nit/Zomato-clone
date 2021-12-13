da3bc86723fc7131d05cb930c8bad279ac9b936c
Arjun kumar <93057535+arjun-kumar-nit@users.noreply.github.com>
2021-12-13 21:10:12 +0530

    Add files via upload

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return (
+    <div className="absolute-center bcol">
+      <b>Made by Arjun Kumar</b>
+      <div className="ligel">
+        <sup>*</sup>Content Copied from Zomato Just for Education Purpose
+        <sup>*</sup>
+      </div>
+      <div>
+        By continuing past this page, you agree to our Terms of Service, Cookie
+        Policy, Privacy Policy and Content Policies. All trademarks are
+        properties of their respective owners. 2008-2021 © Zomato™ Ltd. All
+        rights reserved.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
